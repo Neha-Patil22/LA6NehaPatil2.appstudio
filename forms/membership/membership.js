ed3a29b022b85403a2194f61999c07a57ac147ea
Neha-Patil22 <nbp30352@creighton.edu>
2021-10-02 20:38:40 -0500

    Final

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,3 +1,4 @@
+/*
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
 btnSubmit.onclick = function(){
@@ -11,3 +12,4 @@ if (members.includes(username))
 console.log(members)
 console.log("Neha")
 }
+*/
---
+++ forms/newControls/newControls.js
@@ -0,0 +1,13 @@
+
+Card1_header.innerHTML = "New Header Text"
+Card1_title.innerHTML = "New Title Text"
+Card1_subtitle.innerHTML = "New Subtitle Text"
+Card1_text.innerHTML = "New Text"
+Card1_footer.innerHTML = "New Footer Text"
+
+Card1.subtitleValue = "New Subtitle Text"
+
+/*
+Button3.onclick() =function(){
+ Toast1.toast("show")
+  }*/
